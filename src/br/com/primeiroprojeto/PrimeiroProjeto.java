7b3fc324743d91f45ce12d7335403c101ef20d06
LucasBS10 <lucasbernardo2604@gmail.com>
2023-03-01 20:50:13 -0300

    FIRT COMMIT

---
+++ src/br/com/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,22 @@
+package br.com.primeiroprojeto;
+
+import br.com.primeiroprojeto.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+		//System.out.println(livro);
+		
+		/*
+		int a = 2;
+		int b = 3;
+		int soma = a + b;
+		System.out.println("Olá Mundo! " + soma);*/
+
+	}
+
+}
